36f76ced27796d64464c9d8d449ca0558790af84
MW <michalpawelwierzbicki@gmail.com>
2024-10-06 21:32:52 +0200

    Last two tests

--- test_quicksort.py
+++ test_quicksort.py
@@ -15,5 +15,11 @@ class TestQuicksort(unittest.TestCase):
     def test_duplicates(self):
         self.assertEqual(quicksort([5,1,2,1,5,3]), [1,1,2,3,5,5])
 
+    def test_empty(self):
+        self.assertEqual(quicksort([]), [])
+
+    def test_identical_elements(self):
+        self.assertEqual(quicksort([4, 4, 4, 4]), [4, 4, 4, 4])
+
 if __name__ == '__main__':
     unittest.main()
