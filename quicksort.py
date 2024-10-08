caf6af393697419aae5dc1e8e2d3bc84c2fed59b
ogSikma <mojkero@gmail.com>
2024-10-08 15:52:47 +0200

    Revert "Optimization of the code"
    This reverts commit 0ce6b733448a8637886d67f96521b9a4eaaba237.

--- quicksort.py
+++ quicksort.py
@@ -9,6 +9,6 @@ def quicksort(arr):
 
     left = [x for x in arr[1:] if x < pivot]
 
-    right = [x for x in arr[1:] if x > pivot]
+    right = [x for x in arr[1:] if x >= pivot]
 
     return quicksort(left) + [pivot] + quicksort(right)
